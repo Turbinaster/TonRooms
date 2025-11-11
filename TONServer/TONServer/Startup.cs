12fccbbff3790b764d07808387c53e05b72d3bd3
Turbinaster <83431184+Turbinaster@users.noreply.github.com>
2025-11-11 16:38:00 +0300

    Fix mixed content by enforcing HTTPS URLs

--- TONServer/TONServer/Startup.cs
+++ TONServer/TONServer/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -22,9 +23,9 @@ using System.Threading.Tasks;
 namespace TONServer
 {
     /// <summary> 
-    /// Класс Startup является входной точкой в приложение ASP.NET Core. Этот класс производит конфигурацию приложения, настраивает сервисы, которые приложение будет использовать, устанавливает компоненты для обработки запроса или middleware.
-    /// <para>При запуске приложения сначала срабатывает конструктор, затем метод ConfigureServices() и в конце метод Configure(). Эти методы вызываются средой выполнения ASP.NET.</para>
-    /// <para>Можно создать конструктор без параметров, а можно в качестве параметров передать сервисы IWebHostEnvironment (передает информацию о среде, в которой запускается приложение) и IConfiguration (передает конфигурацию приложения), которые доступны для приложения по умолчанию. К примеру, можно получить доступный для приложения по умолчанию сервис IWebHostEnvironment, сохранить его в переменную и использовать при обработке запроса</para>
+    ///  Startup      ASP.NET Core.     ,  ,    ,       middleware.
+    /// <para>     ,   ConfigureServices()     Configure().      ASP.NET.</para>
+    /// <para>    ,        IWebHostEnvironment (   ,    )  IConfiguration (  ),      .  ,         IWebHostEnvironment,         </para>
     /// </summary>
     public class Startup
     {
@@ -37,7 +38,7 @@ namespace TONServer
             this.env = env;
         }
 
-        /// <summary> Необязательный метод ConfigureServices() регистрирует сервисы, которые используются приложением. </summary>
+        /// <summary>   ConfigureServices()  ,   . </summary>
         public void ConfigureServices(IServiceCollection services)
         {
             _Singleton.ConnectionString = conf.GetConnectionString(env.IsDevelopment() ? "conn" : "conn1");
@@ -48,7 +49,7 @@ namespace TONServer
                 options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
             });
 
-            // установка конфигурации подключения
+            //   
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options => //CookieAuthenticationOptions
                 {
@@ -56,23 +57,23 @@ namespace TONServer
                     options.ExpireTimeSpan = TimeSpan.FromDays(365);
                 });
 
-            //Добавляет только те сервисы фреймворка MVC, которые позволяют использовать контроллеры и представления и связанную функциональность.
+            //     MVC,         .
             services.AddControllersWithViews();
-            //Все сессии работают поверх объекта IDistributedCache, и ASP.NET Core предоставляет встроенную реализацию IDistributedCache, которую мы можем использовать.
+            //     IDistributedCache,  ASP.NET Core    IDistributedCache,    .
             services.AddDistributedMemoryCache(); services.AddSession();
 
             services.AddSignalR();
         }
 
         /// <summary>
-        /// Метод Configure устанавливает, как приложение будет обрабатывать запрос. Этот метод является обязательным. В принципе в метод Configure в качестве параметра может передаваться любой сервис, который зарегистрирован в методе ConfigureServices или который регистрируется для приложения по умолчанию (например, IWebHostEnvironment).
-        /// <para>Обработка запроса в ASP.NET Core устроена по принципу конвейера. Сначала данные запроса получает первый компонент в конвейере. После обработки он передает данные HTTP-запроса второму компоненту и так далее. Эти компоненты конвейера, которые отвечают за обработку запроса, называются middleware. Компонент middleware может либо передать запрос далее следующему в конвейере компоненту, либо выполнить обработку и закончить работу конвейера. Также компонент middleware в конвейере может выполнять обработку запроса как до, так и после следующего в конвейере компонента. Компоненты middleware конфигурируются с помощью методов расширений Run, Map и Use объекта IApplicationBuilder, который передается в метод Configure() класса Startup. Каждый компонент может быть определен как анонимный метод (встроенный inline компонент), либо может быть вынесен в отдельный класс. Для создания компонентов middleware используется делегат RequestDelegate, который выполняет некоторое действие и принимает контекст запроса: public delegate Task RequestDelegate(HttpContext context);</para>
-        /// <para>Метод Configure выполняется один раз при создании объекта класса Startup, и компоненты middleware создаются один раз и живут в течение всего жизненного цикла приложения. То есть для последующей обработки запросов используются одни и те же компоненты.</para>
-        /// <para>Метод Run представляет собой простейший способ для добавления компонентов middleware в конвейер. Однако компоненты, определенные через метод Run, не вызывают никакие другие компоненты и дальше обработку запроса не передают. Метод Use также добавляет компоненты middleware, которые также обрабатывают запрос, но в нем может быть вызван следующий в конвейере запроса компонент middleware. Метод Map (и методы расширения MapXXX()) применяется для сопоставления пути запроса с определенным делегатом, который будет обрабатывать запрос по этому пути. </para>
-        /// <para>Кроме использования делегатов в методах Run/Use/Map мы можем создавать свои компоненты middleware в виде отдельных классов, которые затем добавляются в конвейер с помощью метода UseMiddleware(). </para>
+        ///  Configure ,     .    .     Configure       ,     ConfigureServices        (, IWebHostEnvironment).
+        /// <para>   ASP.NET Core    .        .      HTTP-     .   ,     ,  middleware.  middleware         ,       .   middleware        ,       .  middleware      Run, Map  Use  IApplicationBuilder,     Configure()  Startup.         ( inline ),       .    middleware   RequestDelegate,        : public delegate Task RequestDelegate(HttpContext context);</para>
+        /// <para> Configure        Startup,   middleware           .            .</para>
+        /// <para> Run        middleware  .  ,    Run,           .  Use    middleware,    ,            middleware.  Map (   MapXXX())        ,       . </para>
+        /// <para>     Run/Use/Map      middleware    ,         UseMiddleware(). </para>
         /// </summary>
-        /// <param name="app">Для установки компонентов, которые обрабатывают запрос, используются методы объекта IApplicationBuilder.</param>
-        /// <param name="env">Позволяет получить информацию о среде, в которой запускается приложение, и взаимодействовать с ней.</param>
+        /// <param name="app">  ,   ,    IApplicationBuilder.</param>
+        /// <param name="env">    ,    ,    .</param>
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             Helper.AppName = Assembly.GetExecutingAssembly().GetName().Name;
@@ -95,19 +96,19 @@ namespace TONServer
                 }
             }
 
-            //Выводит подробные сообщения об ошибках
+            //    
             app.UseDeveloperExceptionPage();
-            //Заголовок Strict-Transport-Security сообщает браузеру, что к приложению надо обращаться по протоколу https, а не по протоколу http.
+            // Strict-Transport-Security  ,        https,     http.
             if (!env.IsDevelopment()) app.UseHsts();
-            //Добавляет для проекта переадресацию на тот же ресурс только по протоколу https (если приложение имеет поддержку SSL)
+            //           https (    SSL)
             app.UseHttpsRedirection();
-            //Добавляет поддержку статических файлов
+            //   
             app.UseStaticFiles(new StaticFileOptions
             {
                 ServeUnknownFileTypes = true,
                 DefaultContentType = "text/html"
             });
-            //Добавляет механизм работы с сессиями
+            //    
             app.UseSession();
 
             app.UseAuthentication();
@@ -116,15 +117,20 @@ namespace TONServer
                 RequestCultureProviders = new List<IRequestCultureProvider> { new AcceptLanguageHeaderRequestCultureProvider() }
             });
 
-            //Вызов app.UseRouting() добавляет некоторые возможности маршрутизации, благодаря чему приложение может соотносить запросы с определенными маршрутами.
+            // app.UseRouting()    ,         .
             app.UseRouting();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedProto
+            });
 
             app.UseAuthorization();
 
-            //Позволяет определить маршруты, которые будут обрабатываться приложением
+            //  ,    
             app.UseEndpoints(endpoints =>
             {
-                //Встраивается система маршрутизации, которая позволяет связать приходящие от пользователей запросы с контроллерами.
+                //  ,         .
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=index}/{action=index}/{id?}");
